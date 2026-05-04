132b1e477bfbbe8d4c6e8460b2a5437587651823
unknown <tatitaye0@gmail.com>
2026-05-04 09:31:47 +0300

    first

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // CORS
-const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173', 'http://localhost:5174','*'];
+const allowedOrigins = [process.env.FRONTEND_URL || 'http://localhost:5173', 'https://budget-front-jxuu.onrender.com','*'];
 app.use(cors({
   origin: allowedOrigins,
   credentials: true
